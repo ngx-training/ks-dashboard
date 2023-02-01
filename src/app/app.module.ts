7dfb7e64b4c538da9cf8f97e9b91503984c87aa9
Gregor Doroschenko <g.doroschenko@gmail.com>
2023-02-01 09:46:04 +0100

    Kommentare in AppModule, index html removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // Hier werden alle Komponenten, Direktiven, Pipes importiert
   declarations: [
     AppComponent
   ],
+  // Hier werden alle Module importiert
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  // Hier werden die Service, Logiken importiert
   providers: [],
+  // Hier wird die Komponenten importiert, die für die Ausführung der Anwendung notwending
   bootstrap: [AppComponent]
 })
 export class AppModule { }
