100184d443e1f0dfe93cb2e7798441b7dd351043
Gregor Doroschenko <g.doroschenko@gmail.com>
2023-02-02 13:38:35 +0100

    Add form to login page

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 
-
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    InputTextModule,
+    ButtonModule,
+    FormsModule
   ],
   exports: [
     LoginComponent
