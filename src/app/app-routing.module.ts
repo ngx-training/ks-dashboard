df6a27aef45836bfdfe0975131935c9251167441
Gregor Doroschenko <g.doroschenko@gmail.com>
2023-02-02 11:34:43 +0100

    Add intro route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IntroComponent } from './intro/intro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'intro'
+  },
+  {
+    path: 'intro',
+    component: IntroComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'intro'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
