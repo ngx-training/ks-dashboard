c7383cee6343a5f3e0d5823e5854735859053002
Gregor Doroschenko <g.doroschenko@gmail.com>
2023-02-02 15:12:09 +0100

    Add reactive forms

--- src/app/dashboard/books/book-create/book-create.component.ts
+++ src/app/dashboard/books/book-create/book-create.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-book-create',
   templateUrl: './book-create.component.html',
   styleUrls: ['./book-create.component.css']
 })
-export class BookCreateComponent {
+export class BookCreateComponent implements OnInit {
+
+  createForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.createForm = this.buildForm();
+  }
+
+  buildForm(): FormGroup {
+    return new FormGroup({
+      title: new FormControl('Default title', [Validators.required]),
+      description: new FormControl('Default description', [Validators.required, Validators.minLength(50)]),
+      author: new FormControl(null, [Validators.required]),
+      genre: new FormControl(null),
+      publish_date: new FormControl(null),
+      price: new FormControl(0.0),
+      isAvailable: new FormControl(true)
+    });
+  }
 
 }
--- src/app/dashboard/books/book-create/book-create.module.ts
+++ src/app/dashboard/books/book-create/book-create.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookCreateComponent } from './book-create.component';
-
-
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @NgModule({
   declarations: [
     BookCreateComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    InputTextModule,
+    ButtonModule,
+    ReactiveFormsModule,
+    InputTextareaModule,
+    CheckboxModule
   ],
   exports: [
     BookCreateComponent
