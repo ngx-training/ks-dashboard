6adfa7b40be709280de2e5b5d506d067616233e4
Gregor Doroschenko <g.doroschenko@gmail.com>
2023-02-02 15:18:02 +0100

    Add form group and form array

--- src/app/dashboard/books/book-create/book-create.component.ts
+++ src/app/dashboard/books/book-create/book-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-book-create',
@@ -10,19 +10,29 @@ export class BookCreateComponent implements OnInit {
 
   createForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
   ngOnInit(): void {
     this.createForm = this.buildForm();
   }
 
   buildForm(): FormGroup {
-    return new FormGroup({
-      title: new FormControl('Default title', [Validators.required]),
-      description: new FormControl('Default description', [Validators.required, Validators.minLength(50)]),
-      author: new FormControl(null, [Validators.required]),
-      genre: new FormControl(null),
-      publish_date: new FormControl(null),
-      price: new FormControl(0.0),
-      isAvailable: new FormControl(true)
+    return this.formBuilder.group({
+      title: ['Default title', [Validators.required]],
+      description: ['Default description', [Validators.required, Validators.minLength(50)]],
+      author: [null, [Validators.required]],
+      genre: [null],
+      publish_date: [null],
+      price: [0.0],
+      isAvailable: [true],
+      unterGruppe: this.formBuilder.group({
+        test: [null]
+      }),
+      categories: this.formBuilder.array([
+        this.formBuilder.group({
+          name: [null]
+        })
+      ])
     });
   }
 
